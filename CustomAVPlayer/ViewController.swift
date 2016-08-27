a2d47373f602333880fa9e9571381f93c41447b3
Jake Young <need2edit@gmail.com>
2016-08-27 15:15:44 -0400

    Initial Commit

---
+++ CustomAVPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomAVPlayer
+//
+//  Created by Jake Young on 8/27/16.
+//  Copyright © 2016 Jake Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
