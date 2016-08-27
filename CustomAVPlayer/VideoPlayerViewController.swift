b5499a104ba8b755ba13980081f1af2a7edf3844
Jake Young <need2edit@gmail.com>
2016-08-27 15:28:20 -0400

    Adds video playback using an HLS url, forces landscape, plays automatically

--- CustomAVPlayer/VideoPlayerViewController.swift
+++ CustomAVPlayer/VideoPlayerViewController.swift
@@ -4,32 +4,45 @@
 //
 //  Created by Jake Young on 8/27/16.
 //  Copyright © 2016 Jake Young. All rights reserved.
-//
+//  Code from the tutorial at http://binarymosaic.com/custom-video-player-for-ios-with-avfoundation/
 
 import UIKit
+import AVFoundation
 
 class VideoPlayerViewController: UIViewController {
 
+    let avPlayer = AVPlayer()
+    var avPlayerLayer: AVPlayerLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.backgroundColor = .black
+        
+        // An AVPlayerLayer is a CALayer instance to which the AVPlayer can
+        // direct its visual output. Without it, the user will see nothing.
+        avPlayerLayer = AVPlayerLayer(player: avPlayer)
+        view.layer.insertSublayer(avPlayerLayer, at: 0)
+        
+        let url = URL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")
+        let playerItem = AVPlayerItem(url: url!)
+        avPlayer.replaceCurrentItem(with: playerItem)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        avPlayer.play() // Start the playback
     }
-    */
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        // Layout subviews manually
+        avPlayerLayer.frame = view.bounds
+    }
+    
+    // Force the view into landscape mode (which is how most video media is consumed.)
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
 
 }
